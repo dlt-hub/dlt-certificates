6ccca08770ea196e5ffd024cc36fd566ea84ebab
Alena <alena@dlthub.com>
2025-03-12 18:39:06 +0100

    march certification, 1 batch

---
+++ certificates/technical_certification/bcfcb1eceeb30e2ea1f4cce6ce873e775aaea0e688be8a125c2453eef7445cd5.md
@@ -0,0 +1,34 @@
+
+# Certificate of Achievement: dlt ELT Specialist
+
+## Awarded to **Andrew Cabaniss**
+
+![Course Image](../badges/dlt_ELT_specialist.png)
+
+### Certificate Details
+- **Certificate ID**: `bcfcb1eceeb30e2ea1f4cce6ce873e775aaea0e688be8a125c2453eef7445cd5`
+- **Certificate Holder ID**: `ffc3203b075a8a4d1f776253cdcff247fe9bfa1c54de284a121ba481cdab5880`
+
+### Course Information
+- **Course**: [Course ELT with dlt: dlt Fundamentals](https://github.com/dlt-hub/dlthub-education/tree/main/courses/dlt_fundamentals_dec_2024)
+
+### Issued by
+[**dltHub**](https://dlthub.com/) 
+
+### Certification Period
+- **Issued**: March 2025
+- **Valid Until**: No expiration
+
+---
+
+## Contact Information
+- **GitHub**: acabaniss
+- **Contact**: andrew.cabaniss@fastmail.com
+
+## Comments
+Andrew Cabaniss has successfully completed the Course ELT with dlt: dlt Fundamentals. We commend their dedication and expertise in the field.
+
+---
+
+For more information, please visit [dltHub](https://dlthub.com/).
+    
--- scripts/get_users_to_certificate.py
+++ scripts/get_users_to_certificate.py
@@ -1,23 +1,15 @@
 import argparse
 import os
 import sys
-from typing import Any, Iterator, List
+from typing import Any, Iterator
 
-import pandas as pd
-import pendulum
 from dotenv import load_dotenv
 from google.oauth2.service_account import Credentials
 from googleapiclient.discovery import build
+from users_info import generate_users_info
 
 sys.path.append(os.path.abspath("../"))
-from scripts.utils import generate_hash, save_info_as_json
-
-load_dotenv()
-# set -a && source .env && set +a
-SHEET_ID = os.getenv("SHEET_ID")
-SHEET_NAME = os.getenv("SHEET_NAME")
-CREDENTIALS_INFO = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
-SALT = os.getenv("SALT")
+from utils import save_info_as_json
 
 
 def _initialize_sheets(credentials: Any) -> Any:
@@ -57,42 +49,6 @@ def get_credentials(credentials_info: str | dict) -> Any:
     return credentials
 
 
-def get_both_passed_users(user_data: Iterator) -> pd.DataFrame:
-    df = pd.DataFrame(user_data)
-    part1 = df.iloc[:, :8]
-    part2 = df.iloc[:, 9:14]
-    both = part1.merge(
-        part2, how="inner", left_on="email_1", right_on="email_2"
-    ).drop_duplicates(subset=["email_1"], keep="last")
-    both["passed_at_2"] = both["passed_at_2"].apply(
-        lambda x: pendulum.parse(x, strict=False).isoformat()
-    )
-    print(both.loc[:, "email_1"])
-    return both
-
-
-def transform_users_with_uid(df: pd.DataFrame) -> List[dict]:
-    personal_info = []
-    for i, row in df.iterrows():
-        unique_user_id = generate_hash(row["email_1"], SALT)
-        level = int(row.get("stars", 0))
-        if level == 0:
-            continue
-
-        user = {
-            "certificate_holder_id": unique_user_id,
-            "user_name": row["name_1"],
-            "level": level,
-            "passed_at": row["passed_at_2"],
-            "email": row["email_1"],
-            "github": row["github_1"],
-            "contact": row["contact_1"],
-        }
-        personal_info.append(user)
-
-    return personal_info
-
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         prog="User Details Extractor",
@@ -101,7 +57,15 @@ if __name__ == "__main__":
     parser.add_argument(
         "output_file", type=str, help="Output file path, e.g. 'people.json'"
     )
+    parser.add_argument("--env_path", default=".env", type=str, help="Env file path")
     args = parser.parse_args()
+
+    load_dotenv(args.env_path)
+    # set -a && source .env && set +a
+    SHEET_ID = os.getenv("SHEET_ID")
+    SHEET_NAME = os.getenv("SHEET_NAME")
+    CREDENTIALS_INFO = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
+    SALT = os.getenv("SALT")
 
     sheet_data = get_sheet(
         credentials=get_credentials(CREDENTIALS_INFO),
@@ -109,7 +73,6 @@ if __name__ == "__main__":
         sheet_name=SHEET_NAME,
     )
 
-    users_df = get_both_passed_users(sheet_data)
-    users_info = transform_users_with_uid(users_df)
+    users_info = generate_users_info(sheet_data, SALT)
     print(f"{len(users_info)} users extracted")
     save_info_as_json(users_info, args.output_file)
